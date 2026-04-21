03decd7023b3b45911d3fe0a0b9f1dac506c6300
Jaël Bennett <jael.andre97@gmail.com>
2026-04-21 18:29:20 -0400

    Update frontend API URL for EC2 deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   const [imageLabels, setImageLabels] = useState([]);
   const [selectedImage, setSelectedImage] = useState(null);
 
-  const API_BASE_URL = "http://localhost:5001";
+  const API_BASE_URL = "http://52.90.143.126:5001";
 
   const handleLogin = async () => {
     try {
